d8f257ef05044ff8fec0845e5ae1715b3e0fe7d1
kuande122 <95857538+kuande122@users.noreply.github.com>
2021-12-30 19:50:37 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,15 @@
 import csv
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import RendererAgg
+from matplotlib.figure import Figure
+import seaborn as sns
+import statsmodels
+from statsmodels.nonparametric.smoothers_lowess import lowess
 st.subheader('1.讀入資料')
 df = pd.read_csv('https://github.com/kuande122/project/blob/main/2020.csv')
 st.dataframe(df)
