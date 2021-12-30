d345f253266df7dfa547ed24d4db10b3a9025aca
kuande122 <95857538+kuande122@users.noreply.github.com>
2021-12-30 21:02:11 +0800

    Update project.py

--- project.py
+++ project.py
@@ -6,6 +6,7 @@ from PIL import Image
 data = pd.read_csv('2020.csv')
 st.write("## THE DATA BEING USED")
 data
+st.image(Cpbl-stats-chinatrust-brothers.png)
 option = st.selectbox( '選擇球隊？', ['中信兄弟', '統一7-Eleven獅', '味全龍', '樂天桃猿','富邦悍將'])
 if option == 中信兄弟
   st.image(Cpbl-stats-chinatrust-brothers.png)
