ad4c5d7a59c4549fd13589053234aabf6ddac70c
kuande122 <95857538+kuande122@users.noreply.github.com>
2021-12-30 19:54:49 +0800

    Update project.py

--- project.py
+++ project.py
@@ -2,9 +2,6 @@ import csv
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import statsmodels
-from statsmodels.nonparametric.smoothers_lowess import lowess
 st.subheader('1.讀入資料')
 df = pd.read_csv('https://github.com/kuande122/project/blob/main/2020.csv')
 st.dataframe(df)
