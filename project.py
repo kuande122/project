00d756d0b30340bce251c08eb26b6bb759da245c
kuande122 <95857538+kuande122@users.noreply.github.com>
2021-12-30 19:52:56 +0800

    Update project.py

--- project.py
+++ project.py
@@ -2,7 +2,6 @@ import csv
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import RendererAgg
