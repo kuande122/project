773a6398681528887fb59bd1ba0207b6bbf2f3d7
kuande122 <95857538+kuande122@users.noreply.github.com>
2021-12-30 20:19:53 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import streamlit as st  
 from st_aggrid import AgGrid
 import pandas as pd
-st.sucess('如以下這是你的表格')
-df=pd.read_csv('2020.csv')
-AgGrid(df)
+data = pd.read_csv('2020.csv')
+st.write("## THE DATA BEING USED")
+data
