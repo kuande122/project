62d79e6893081f502f89d2689c31a76c459464cc
kuande122 <95857538+kuande122@users.noreply.github.com>
2021-12-30 20:51:31 +0800

    Update project.py

--- project.py
+++ project.py
@@ -6,3 +6,5 @@ from PIL import Image
 data = pd.read_csv('2020.csv')
 st.write("## THE DATA BEING USED")
 data
+selected_data = st.selectbox('Select a Player', options=records,
+        format_func=lambda record: f'{record["full_name"]}')
