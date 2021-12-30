63c52b15bbdec0258ef7b6e1b3ce283a05f1294f
kuande122 <95857538+kuande122@users.noreply.github.com>
2021-12-30 20:20:41 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
-import streamlit as st  
-from st_aggrid import AgGrid
+import streamlit as st
 import pandas as pd
+import altair as alt
+import numpy as np
+from PIL import Image
 data = pd.read_csv('2020.csv')
 st.write("## THE DATA BEING USED")
 data
