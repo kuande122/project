03d75fb2aa18ceb5ac08607f80c15cadab0116b8
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-06 21:31:06 +0800

    Update project.py

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt   # 資料視覺化套件
 from PIL import Image
 wang = pd.read_excel('wang.xlsx')
 st.write(wang)    # 顯示前3筆資料
-plt.plot(wang.SEASON_ID, wang.打擊率, color='b')
+plt.plot(wang.年度, wang.打擊率, color='b')
 plt.xlabel('SEASON') # 設定x軸標題
 plt.xticks(wang.SEASON_ID, rotation='vertical') # 設定x軸label以及垂直顯示
 plt.title('wang') # 設定圖表標題
