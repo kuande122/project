2213e6e952cea090398cce5164a4f07999586897
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 00:16:46 +0800

    Update project.py

--- project.py
+++ project.py
@@ -2,10 +2,19 @@ import streamlit as st
 import pandas as pd               # 資料處理套件
 import matplotlib.pyplot as plt   # 資料視覺化套件
 from PIL import Image
+st.set_page_config(
+    page_title="猛祺的期末報告",
+    page_icon="st.image(phil)",
+    
+    layout="centered",
+    initial_sidebar_state="collapsed",
+)
+
 st.title('中華職棒數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
 option = st.sidebar.selectbox( '選擇球隊？', ['中信兄弟', '統一7-Eleven獅', '味全龍', '樂天桃猿','富邦悍將'])
 option1 = st.sidebar.selectbox( '選擇所想查看的數據？', ['球隊成績', '投手成績', '打擊成績', '守備成績'])
+
 if option == '中信兄弟':
   image = Image.open('brothers.png')
   st.image(image)
@@ -45,12 +54,16 @@ elif option == '統一7-Eleven獅':
     st.header('守備成績')
     lc=pd.read_excel('lc.xlsx')
     st.write(lc) 
+    
 elif option == '味全龍':
   image = Image.open('Dragons.png')
   st.image(image)
+  
 elif option == '樂天桃猿':
   image = Image.open('Rakuten.png')
   st.image(image)
+  
+  
 elif option == '富邦悍將':
   image = Image.open('guardians.png')
   st.image(image)
