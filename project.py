02516d8ccb6527ba5d3b0aea3c74b2a2e13acf19
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-06 21:30:30 +0800

    Update project.py

--- project.py
+++ project.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt   # 資料視覺化套件
 from PIL import Image
 wang = pd.read_excel('wang.xlsx')
 st.write(wang)    # 顯示前3筆資料
+plt.plot(wang.SEASON_ID, wang.打擊率, color='b')
+plt.xlabel('SEASON') # 設定x軸標題
+plt.xticks(wang.SEASON_ID, rotation='vertical') # 設定x軸label以及垂直顯示
+plt.title('wang') # 設定圖表標題
+plt.show()
 option = st.sidebar.selectbox( '選擇球隊？', ['中信兄弟', '統一7-Eleven獅', '味全龍', '樂天桃猿','富邦悍將'])
 if option == '中信兄弟':
   image = Image.open('brothers.png')
