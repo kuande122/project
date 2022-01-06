d1ee544f69e40e95ba9d0eadf25a2847efc76660
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 01:57:49 +0800

    Update project.py

--- project.py
+++ project.py
@@ -23,20 +23,7 @@ if option == '中信兄弟':
     st.header('投手成績')
     bp=pd.read_excel('bp.xlsx')
     st.write(bp)
-                   # 使用ggplot主題樣式
-
-    #畫第一條線，plt.plot(x, y, c)參數分別為x軸資料、y軸資料及線顏色 = 紅色
-    plt.plot(bp["年度"], bp["防禦率"],c = "r")  
-    # 設定圖例，參數為標籤、位置
-    plt.xlabel("年度", fontweight = "bold")                # 設定x軸標題及粗體
-    plt.ylabel("防禦率", fontweight = "bold")    # 設定y軸標題及粗體
-    plt.title("年度防禦率折線圖", fontsize = 15, fontweight = "bold", y = 1.1)   # 設定標題、文字大小、粗體及位置
-    plt.xticks(rotation=45)   # 將x軸數字旋轉45度，避免文字重疊
-
-    plt.savefig("中信兄弟年度防禦率折線圖.jpg",   # 儲存圖檔
-            bbox_inches='tight',               # 去除座標軸占用的空間
-            pad_inches=0.0)                    # 去除所有白邊
-    st.pyplot(plt)
+                 
    
   elif option1=='打擊成績':
     st.header('打擊成績')
