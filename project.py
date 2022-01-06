13503e1c121cec2e7638774a86bb4e042e1ed977
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-06 21:47:59 +0800

    Update project.py

--- project.py
+++ project.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd               # 資料處理套件
 import matplotlib.pyplot as plt   # 資料視覺化套件
 from PIL import Image
-wang = pd.read_excel('wang.xlsx')
-st.write(wang)    # 顯示前3筆資料
 plt.plot(wang.年度, wang.打擊率, color='b')
 plt.xlabel('SEASON') # 設定x軸標題
 plt.xticks(wang.年度, rotation='vertical') # 設定x軸label以及垂直顯示
@@ -13,9 +11,13 @@ option = st.sidebar.selectbox( '選擇球隊？', ['中信兄弟', '統一7-Elev
 if option == '中信兄弟':
   image = Image.open('brothers.png')
   st.image(image)
+  bp = pd.read_excel('bp.xlsx')
+  st.write(bp)   
 elif option == '統一7-Eleven獅':
   image = Image.open('unilion.png')
   st.image(image)
+  lp = pd.read_excel('lp.xlsx')
+  st.write(lp)
 elif option == '味全龍':
   image = Image.open('Dragons.png')
   st.image(image)
