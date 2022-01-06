755b137b8eabc7778e96bf8919907b89c375aece
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 02:22:32 +0800

    Update project.py

--- project.py
+++ project.py
@@ -36,7 +36,7 @@ if option == '中信兄弟':
     plt.xticks(bt.年度, rotation='vertical') # 設定x軸label以及垂直顯示
     plt.xticks(lt.年度, rotation='vertical') # 設定x軸label以及垂直顯示
     plt.title('CTBC Brothers Batting AVG and VS Other Teams ') # 設定圖表標題
-    plt.legend(labels=["CTBC Brothers", "Unilions"], loc = 'best'))
+    
     st.pyplot(plt)
     
   else:
