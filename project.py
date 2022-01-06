c495d43c02ae441de0e1728c3779f16661d9e153
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 05:13:29 +0800

    Update project.py

--- project.py
+++ project.py
@@ -45,15 +45,7 @@ if option == '中信兄弟':
     st.pyplot(plt) 
     
     
-    plt.bar(BrothersPitching.奪三振.unique(),
-        width=0.5, 
-        bottom=None, 
-        align='center', 
-        color=['lightsteelblue', 
-               'cornflowerblue'])
-    plt.xticks(rotation='vertical')
-    plt.title('2021 CTBC Brothers Strike Out  VS Other Teams ')
-    st.pyplot(plt) 
+ 
     
     
     
