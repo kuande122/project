85f34ee99d0bbfe05e126f8dade68dacf0628e27
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 01:12:27 +0800

    Update project.py

--- project.py
+++ project.py
@@ -25,8 +25,7 @@ if option == '中信兄弟':
     chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
+    st.line_chart(chart_data)
    
   elif option1=='打擊成績':
     st.header('打擊成績')
