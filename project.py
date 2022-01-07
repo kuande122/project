77afa93184b14350a2a756d3abd29e6374255f75
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 13:13:29 +0800

    Update project.py

--- project.py
+++ project.py
@@ -43,9 +43,6 @@ plt.xticks(DragonsPitching.年度)
 plt.title('CTBC Brothers Pitching ERA VS Other Teams ',{'fontsize':10}) # 設定圖表標題
 plt.legend(labels=["BrothersPitching", "UnilionsPitching","DragonsPitching","GuardiansPitching","RakutenPitching"], loc = 'best')
 st.pyplot(plt)
-
-plt.tight_layout()
-
 st.header('數據分析')
 plt.subplot(2, 1 ,2)
 plt.style.use("ggplot") 
@@ -64,6 +61,13 @@ plt.xticks(DragonsBatting.年度)
 plt.title('CTBC Brothers Batting Avg VS Other Teams ',{'fontsize':10}) # 設定圖表標題
 plt.legend(labels=["BrothersBatting", "UnilionsBatting","DragonsBatting","GuardiansBatting","RakutenBatting"], loc = 'best')
 st.pyplot(plt)
+plt.tight_layout()
+plt.subplots_adjust(left=0.125,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.2, 
+                    hspace=0.35)
     
 
 
