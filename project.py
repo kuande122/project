ef1d93a6fe26095cc810c67144cce01aacc27742
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 21:21:18 +0800

    Update project.py

--- project.py
+++ project.py
@@ -12,8 +12,8 @@ fig ,ax = plt.subplots(figsize=(12,8))
 ax.pie(wind,
       labels=win,
       autopct='%.1f%%', # 比例格式
-      explode=separeted,
-      shadow=True# 凸顯
+      
+     
       ), # 陰影
 ax.set_title("haha",fontsize=20)
 ax.legend(win, loc=3, fontsize='small')
