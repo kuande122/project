f2bd73026e66377856f5acedc2a2fddf97c4eaa9
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 21:46:27 +0800

    Update project.py

--- project.py
+++ project.py
@@ -10,6 +10,7 @@ explode = (0,0,0.1 ,0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
 	shadow=True, startangle=90)
+plt.legend(loc = "best")   
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 st.pyplot(fig1)
