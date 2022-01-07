1ccbcf6a1b6c2b8b9f71d7a71933cd4eacc58dce
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 21:51:02 +0800

    Update project.py

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ explode = (0,0,0.1 ,0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-	shadow=True, startangle=90,textprops = {"fontsize" : 12})
+	shadow=True, startangle=90,textprops = {"fontsize" : 5})
 plt.title("Pie chart of 2021 Full Year Record", {"fontsize" : 18})
 plt.legend(loc = "best")   
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
