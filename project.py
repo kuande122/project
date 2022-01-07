50b364cd1f574b0f99133a5b1aff7d6bf7381c8c
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 10:06:11 +0800

    Update project.py

--- project.py
+++ project.py
@@ -99,21 +99,21 @@ if option == '中信兄弟':
     plt.legend(labels=["BrothersBatting", "UnilionsBatting","DragonsBatting","GuardiansBatting","RakutenBatting"], loc = 'best')
     st.pyplot(plt)
     if option1=='球隊成績':
-    plt.plot(BrothersBatting.年度, BrothersBatting.長打率,'.-', color='yellow')
-    plt.plot(UnilionsBatting.年度, UnilionsBatting.長打率,'.-', color='darkorange')
-    plt.plot(DragonsBatting.年度, DragonsBatting.長打率, '.-',color='red')
-    plt.plot(GuardiansBatting.年度, GuardiansBatting.長打率,'.-', color='darkblue')
-    plt.plot(RakutenBatting.年度, RakutenBatting.長打率, '.-',color='maroon')
-    plt.xlabel('Season') # 設定x軸標題
-    plt.xticks([2021,2020,2019,2018,2017,2016,2015,2014])
-    plt.xticks(BrothersBatting.年度) # 設定x軸label以及垂直顯示
-    plt.xticks(UnilionsBatting.年度) 
-    plt.xticks(RakutenBatting.年度) 
-    plt.xticks(GuardiansBatting.年度) 
-    plt.xticks(DragonsBatting.年度) 
-    plt.title('CTBC Brothers Batting SLG VS Other Teams ') # 設定圖表標題
-    plt.legend(labels=["BrothersBatting", "UnilionsBatting","DragonsBatting","GuardiansBatting","RakutenBatting"], loc = 'best')
-    st.pyplot(plt)
+        plt.plot(BrothersBatting.年度, BrothersBatting.長打率,'.-', color='yellow')
+        plt.plot(UnilionsBatting.年度, UnilionsBatting.長打率,'.-', color='darkorange')
+        plt.plot(DragonsBatting.年度, DragonsBatting.長打率, '.-',color='red')
+        plt.plot(GuardiansBatting.年度, GuardiansBatting.長打率,'.-', color='darkblue')
+        plt.plot(RakutenBatting.年度, RakutenBatting.長打率, '.-',color='maroon')
+        plt.xlabel('Season') # 設定x軸標題
+        plt.xticks([2021,2020,2019,2018,2017,2016,2015,2014])
+        plt.xticks(BrothersBatting.年度) # 設定x軸label以及垂直顯示
+        plt.xticks(UnilionsBatting.年度) 
+        plt.xticks(RakutenBatting.年度) 
+        plt.xticks(GuardiansBatting.年度) 
+        plt.xticks(DragonsBatting.年度) 
+        plt.title('CTBC Brothers Batting SLG VS Other Teams ') # 設定圖表標題
+        plt.legend(labels=["BrothersBatting", "UnilionsBatting","DragonsBatting","GuardiansBatting","RakutenBatting"], loc = 'best')
+        st.pyplot(plt)
     
     
   else:
