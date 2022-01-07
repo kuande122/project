e1d16456db6b26e8f4d71972a5578776a54cf299
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 13:26:36 +0800

    Update project.py

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ st.set_page_config(
     )
 st.title('中華職棒數據查詢系統')
 st.sidebar.header('選擇球隊及數據')
-x=st.button('點取看更多分析')
+
 option = st.sidebar.selectbox( '選擇球隊？', ['中信兄弟', '統一7-Eleven獅', '味全龍', '樂天桃猿','富邦悍將'])
 option1 = st.sidebar.selectbox( '選擇所想查看的數據？', ['球隊成績', '投手成績', '打擊成績', '守備成績'])
 BrothersBatting=pd.read_excel('BrothersBatting.xlsx')
