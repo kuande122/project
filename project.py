13de9b359f44151cd1d46a60dc8535675ba59b66
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 21:53:40 +0800

    Update project.py

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 labels = 'Home Win', 'Away Win','Home Lose', 'Away Lose','Home Tie', 'Away Tie'
 sizes = [11, 15, 18, 13,1,2]
-explode = (0,0,0.3 ,0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+explode = (0,0,0.2 ,0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
