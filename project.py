8febdca4fa0f35f4baed8aa2dc8eb960756d1e08
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 21:36:59 +0800

    Update project.py

--- project.py
+++ project.py
@@ -3,12 +3,13 @@ import numpy as np
 import pandas as pd
 
 
-labels = 'Home Win', 'Away Win', 'Home Lose', 'Away Lose','Home Tie','Away Tie'
-sizes = [11, 15, 18, 13,1,2]
-explode = (0, 0,0.1,0,0,0) # only "explode" the 2nd slice (i.e. 'Hogs')
+	labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+	sizes = [15, 30, 45, 10]
+	explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
-fig1,ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-st.pyplot()
+	fig1, ax1 = plt.subplots()
+	ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+	        shadow=True, startangle=90)
+	ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+	st.pyplot()
