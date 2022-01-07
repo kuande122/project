b95605408dff17d280da4cb91f3f0088de6b80f9
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 13:15:55 +0800

    Update project.py

--- project.py
+++ project.py
@@ -66,7 +66,7 @@ plt.subplots_adjust(left=0.125,
                     bottom=0.1, 
                     right=0.9, 
                     top=0.9, 
-                    wspace=0.5, 
+                    wspace=70, 
                     hspace=10)
     
 
