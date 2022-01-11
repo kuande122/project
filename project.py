240f7298976ec45197e5cdbccbbf5678f82eaf16
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-11 13:09:30 +0800

    Update project.py

--- project.py
+++ project.py
@@ -32,7 +32,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(
-     np.random.randn(1000, 2) / [50, 50] + [25.04, 121.44],
+     np.random.randn(1, 2) / [50, 50] + [25.04, 121.44],
      columns=['lat', 'lon'])
 
 st.map(df)
