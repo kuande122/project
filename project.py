2fcad6c2896f75eb83659d42e845dab04543da6c
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-12 00:56:53 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,50 +1,6 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import streamlit as st
-import base64
-file_ = open("rr.gif", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
-st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="rr gif">',
-    unsafe_allow_html=True,
-)
-col1, col2 = st.columns(2)
-col1.metric("季冠軍🏆", "1  次")
-col2.metric("年度冠軍🏆", "10  次")
-labels = 'Home Win', 'Away Win','Home Lose', 'Away Lose','Home Tie', 'Away Tie'
-sizes = [11, 15, 18, 13,1,2]
-explode = (0,0,0.2 ,0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+import folium # 匯入 folium 套件
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-	shadow=True, startangle=90,textprops = {"fontsize" : 7})
-plt.title("Pie chart of 2021 Full Year Record", {"fontsize" : 18})
-plt.legend(loc = "best")   
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-st.pyplot(fig1)
-import streamlit as st
-from streamlit_folium import folium_static
-import folium
-
-page = st.radio("Select map type", ["Single map", "Dual map"], index=0)
-
-# center on Liberty Bell
-if page == "Single map":
-    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-elif page == "Dual map":
-    m = folium.plugins.DualMap(location=[39.949610, -75.150282], zoom_start=16)
-
-# add marker for Liberty Bell
-tooltip = "Liberty Bell"
-folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-).add_to(m)
-
-# call to render Folium map in Streamlit
-folium_static(m)
-
+# 建立地圖與設定位置
+fmap = folium.Map(location=[25.11373,121.53329], zoom_start=16)
+fmap  # 在notebook中顯示地圖
 
