5572d3f7837af5d545b25153940ced5510c3da52
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-12 01:44:38 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,10 +1,22 @@
 import streamlit as st
-import folium # 匯入 folium 套件
+from streamlit_folium import folium_static
+import folium
 
-import datetime
+"# streamlit-folium"
 
-unsafe_allow_html=True
+with st.echo():
+    import streamlit as st
+    from streamlit_folium import folium_static
+    import folium
 
-# 建立地圖與設定位置
-fmap = folium.Map(location=[35.709635, 139.810851], zoom_start=16)
-fmap  # 在notebook中顯示地圖
+    # center on Liberty Bell
+    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+
+    # add marker for Liberty Bell
+    tooltip = "Liberty Bell"
+    folium.Marker(
+        [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
+    ).add_to(m)
+
+    # call to render Folium map in Streamlit
+    folium_static(m)
