b3a43dc57ce8fefac802b0b045b186e0b6eae456
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-12 01:31:45 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import folium # 匯入 folium 套件
 
+unsafe_allow_html=True
+
 # 建立地圖與設定位置
 fmap = folium.Map(location=[35.709635, 139.810851], zoom_start=16)
 fmap  # 在notebook中顯示地圖
