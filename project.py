82ea2e1f99772965ae913e2793bc3d62dcafc813
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-12 01:18:07 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,50 +1,6 @@
-import setuptools
-
-setuptools.setup(
-    name="streamlit_folium",
-    version="0.4.0",
-    author="Randy Zwitch",
-    author_email="randy@streamlit.io",
-    description="Render Folium objects in Streamlit",
-    long_description="",
-    long_description_content_type="text/plain",
-    url="https://github.com/randyzwitch/streamlit-folium",
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    classifiers=[],
-    python_requires=">=3.6",
-    install_requires=["streamlit", "folium"],
-)
-
-
-
-
 import streamlit as st
+import folium # 匯入 folium 套件
 
-st.set_page_config(page_title="streamlit-folium documentation")
-
-"# streamlit-folium"
-
-with st.echo():
-
-    import streamlit as st
-    from streamlit_folium import folium_static
-    import folium
-
-    page = st.radio("Select map type", ["Single map", "Dual map"], index=0)
-
-    # center on Liberty Bell
-    if page == "Single map":
-        m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-    elif page == "Dual map":
-        m = folium.plugins.DualMap(location=[39.949610, -75.150282], zoom_start=16)
-
-    # add marker for Liberty Bell
-    tooltip = "Liberty Bell"
-    folium.Marker(
-        [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-    ).add_to(m)
-
-    # call to render Folium map in Streamlit
-    folium_static(m)
-
+# 建立地圖與設定位置
+fmap = folium.Map(location=[35.709635, 139.810851], zoom_start=16)
+fmap  # 在notebook中顯示地圖
