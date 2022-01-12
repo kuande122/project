ecfc85b6f9e34a5c4604e32bffcf2fff7105ed57
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-12 08:42:45 +0800

    Update project.py

--- project.py
+++ project.py
@@ -1,13 +1,13 @@
- import streamlit as st
- from streamlit_folium import folium_static
- import folium
+import streamlit as st
+from streamlit_folium import folium_static
+import folium
  # center on Liberty Bell
- m = folium.Map(location=[25.04054,121.44768], zoom_start=16)
+m = folium.Map(location=[25.04054,121.44768], zoom_start=16)
 
  # add marker for Liberty Bell
- tooltip = "全富武裝"
- folium.Marker([25.04054,121.44768], popup="全富武裝", tooltip=tooltip
+tooltip = "全富武裝"
+folium.Marker([25.04054,121.44768], popup="全富武裝", tooltip=tooltip
     ).add_to(m)
 
  # call to render Folium map in Streamlit
- folium_static(m)
+folium_static(m)
