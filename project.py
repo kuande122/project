2d5acc63344979a5d75eb5c62acab5e77447234a
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-12 08:45:06 +0800

    Update project.py

--- project.py
+++ project.py
@@ -5,8 +5,17 @@ import folium
 m = folium.Map(location=[25.04054,121.44768], zoom_start=16)
 
  # add marker for Liberty Bell
-tooltip = "全富武裝"
-folium.Marker([25.04054,121.44768], popup="全富武裝", tooltip=tooltip
+tooltip = "新北新莊棒球場"
+folium.Marker([25.04054,121.44768], popup="新北新莊棒球場", tooltip=tooltip
+    ).add_to(m)
+
+ # call to render Folium map in Streamlit
+folium_static(m)
+m1 = folium.Map(location=[24.19978, 120.68498], zoom_start=16)
+
+ # add marker for Liberty Bell
+tooltip = "台中洲際棒球場"
+folium.Marker([24.19978, 120.68498], popup="台中洲際棒球場", tooltip=tooltip
     ).add_to(m)
 
  # call to render Folium map in Streamlit
